9a80a6019975c49cc247e68731276225c42cabe8
joyce <jo824.tw@gmail.com>
2016-08-18 17:38:10 +0800

    Change image url

--- app/src/main/java/com/joyce/kuo/inmacau/view/fragment/MyAdapter.java
+++ app/src/main/java/com/joyce/kuo/inmacau/view/fragment/MyAdapter.java
@@ -9,6 +9,8 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.joyce.kuo.inmacau.R;
 
 import java.util.ArrayList;
@@ -64,8 +66,15 @@ public class MyAdapter extends BaseAdapter {
         View v = inflater.inflate(R.layout.screen, null);
         ImageView img = (ImageView) v.findViewById(R.id.imageView2);
         //取得圖片位置
-        int drawableResourceId = context.getResources().getIdentifier(data.get(position).ID, "drawable", context.getPackageName());
-        img.setImageResource(drawableResourceId);
+        // int drawableResourceId = context.getResources().getIdentifier(data.get(position).ID, "drawable", context.getPackageName());
+        //img.setImageResource(drawableResourceId);
+        Glide.with(context)
+                .load(data.get(position).ID)
+                .override(300,200)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
+                .into(img);
+
         TextView tv1 = (TextView) v.findViewById(R.id.textView2);
         tv1.setText(data.get(position).NAME);
 
