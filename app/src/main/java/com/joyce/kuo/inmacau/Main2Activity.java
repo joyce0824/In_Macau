91002869f41488ec074ded6de9ad44f16d834dc2
joyce <jo824.tw@gmail.com>
2016-08-18 12:09:47 +0800

    menu.2

--- app/src/main/java/com/joyce/kuo/inmacau/Main2Activity.java
+++ app/src/main/java/com/joyce/kuo/inmacau/Main2Activity.java
@@ -5,11 +5,15 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.joyce.kuo.inmacau.list.ListActivity;
 import com.joyce.kuo.inmacau.trip.TripActivity;
@@ -17,12 +21,15 @@ import com.joyce.kuo.inmacau.view.MapActivity;
 
 public class Main2Activity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
 
     }
@@ -66,20 +73,22 @@ public class Main2Activity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        menu.add(0,0,Menu.NONE,"關於我們");
+        menu.add(0,1,Menu.NONE,"聯絡我們");
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == 0) {
+            Toast.makeText(Main2Activity.this, "BB", Toast.LENGTH_SHORT).show();
+            AlertDialog.Builder builder=new AlertDialog.Builder(Main2Activity.this);
+            builder.setTitle("關於我們");
+            builder.setMessage("特別感謝:澳門特別行政區政府旅遊局\n資料來源:來自澳門特別行政區政府旅遊局");
             return true;
         }
 
